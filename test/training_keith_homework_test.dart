69e7f037ae140387a51bdb96587cd42f8738d211
kienvo3105 <keith.vo.goldenowl@gmail.com>
2024-02-07 16:56:58 +0700

    init project

---
+++ test/training_keith_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:training_keith_homework/training_keith_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
